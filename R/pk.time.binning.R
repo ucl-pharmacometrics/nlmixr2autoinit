545c5412b3abae8bd15a1d93d877d895c2a7221c
ZhonghuiHuang <huangzhonghui17@163.com>
2024-09-19 09:15:40 +0100

    debug

--- R/pk.time.binning.R
+++ R/pk.time.binning.R
@@ -19,14 +19,14 @@ pk.time.binning <- function(testdat,
   test.pool <- subset(test.pool, select = c(tad, DVnor))
   colnames(test.pool) <- c("Time", "Conc")
   test.pool <- test.pool[order(test.pool$Time), ]
-  
+
   # Define the number of bins (current 8)
   times.quantiles <- quantile(sort(unique(test.pool$Time)),
                               probs = seq(0, 1,
                                           length.out = nbins))
   # print(times.quantiles)
   adjusted_times.quantiles <- sort(unique(times.quantiles))
-  
+
   # Group the plasma concentration based on the calculated quantiles
   test.pool$group <- cut(
     test.pool$Time,
@@ -34,14 +34,14 @@ pk.time.binning <- function(testdat,
     include.lowest = TRUE,
     labels = FALSE
   )
-  
+
   # Draw vertical split lines for time quantiles
   # find the max time in each groups
   test.pool.group <- aggregate(test.pool$Time,
                                list(test.pool$group),
                                FUN = max,
                                na.rm = T)
-  
+
   # Calculate the median time for each group
   test.pool.subject.time <- aggregate(test.pool$Time,
                                       list(test.pool$group),
@@ -52,21 +52,21 @@ pk.time.binning <- function(testdat,
                                       list(test.pool$group),
                                       FUN = median,
                                       na.rm = T)
-  
-  # Reason for collect the time and concentration seperately, in case collect the extreme concentration.
-  
+
+  # Reason for collect the time and concentration separately, in case collect the extreme concentration.
+
   test.pool.normalised <-
     data.frame(time = test.pool.subject.time$x,
                test.pool.subject.conc$x)
   colnames(test.pool.normalised) <- c("Time", "Conc")
-  
+
   colnames(test.pool.group) <- c("Group", "Upper Time Boundary ")
-  
+
   return(
     list(
       test.pool.normalised = test.pool.normalised,
       test.pool.group = test.pool.group
     )
   )
-  
+
 }
