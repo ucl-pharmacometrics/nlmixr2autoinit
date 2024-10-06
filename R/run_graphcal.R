a8c6e8ca994dcea770e84cf562a085d506d2975c
ZhonghuiHuang <huangzhonghui17@163.com>
2024-10-06 21:04:32 +0100

    add infusion part

--- R/run_graphcal.R
+++ R/run_graphcal.R
@@ -1,52 +1,63 @@
-#' Run graphical calculation
+#' Graphical calculation of pharmacokinetic parameters
 #'
 #' Performs a graphical calculation of clearance and volume of distribution based on provided pharmacokinetic data with concentration normalised by dose
 #'
-#' @param dat A data frame containing the intravenous pharmacokinetic data.
-#' @param fdobsflag A flag indicating whether the dataset includes first-dose observations. Set to 1 if the data includes first-dose observations, otherwise set to 0.
-#' @param nbins The number of bins for time binning.
-#' @param nlastpoints The number of last points to be used for slope linear regression.
+#' @param dat A data frame containing the pharmacokinetic data.
+#' @param route A character string specifying the route of administration. It can be "bolus",
+#' "infusion", or "oral". The function will adapt its calculations based on the specified route.
+#' @param nbins An integer specifying the number of bins used to divide the time points for naive pooling of the data. The default is 8.
+#' @param nlastpoints Numeric value specifying the number of last data points used for linear regression to obtain the slope in the terminal phase. (default is \code{3} in the graphical analysis).
 #' @return A data frame with calculated clearance, volume of distribution, slope, and time spent for the calculation.
 #' @importFrom dplyr %>% mutate if_else group_by ungroup
 #' @import nlmixr2
 #' @examples
-#' # example 1 (iv case)
+#' # Example 1 (iv case)
 #'
 #' dat <- Bolus_1CPT
 #' dat <- nmpkconvert(dat)
 #' dat <- calculate_tad(dat)
-#' run_graphcal(dat, fdobsflag = 1, noniv_flag=0, nbins = 8, nlastpoints = 4)
+#' run_graphcal(dat, route="bolus")
 #'
-#' # example2 (oral case)
+#' Example 2 (oral case)
 #' dat <- Oral_1CPT
 #' dat <- nmpkconvert(dat)
 #' dat <- calculate_tad(dat)
-#' run_graphcal(dat, fdobsflag = 1,  noniv_flag=1, nbins = 8, nlastpoints = 4)
+#' run_graphcal(dat, route="oral")
+#'
+#' Example 3 (infusion case).
+#' Approximate calculation. only use when the infusion duration is very short
+#'
+#' dat <- Infusion_1CPT
+#' dat <- nmpkconvert(dat)
+#' dat <- calculate_tad(dat)
+#' run_graphcal(dat, route="infusion")
+#'
 #' @export
 
 run_graphcal <- function(dat,
-                         fdobsflag,
-                         noniv_flag,
-                         nbins,
-                         nlastpoints) {
+                         route,
+                         nbins=8,
+                         nlastpoints=3) {
 
-  if (missing(fdobsflag)){
-    stop("Whether samples after first dose is available is unknown, please set it by fdobsflag")
-  }
-  if (missing(noniv_flag)){
-    stop("Whether this is a oral case is unknown, please set it bynoniv_flag")
+  if (missing(route)){
+    stop("Error, no dosing route was specified, please set it using the route option.")
   }
 
-  if (noniv_flag==0){
-  graph.fd.results <- data.frame(
-    cl = NA,
-    vd = NA,
-    slope = NA,
-    time.spent = 0
-  )
+  if (!1 %in% dat$dose_number){
+    stop("Error. no sampling point in the first dosing interval was detected")
+  }
 
-  if (fdobsflag == 1) {
-    start.time <- Sys.time()
+  if (route=="bolus"|| route=="infusion"){
+
+  start.time <- Sys.time()
+
+    graph.fd.results <- data.frame(
+      cl = NA,
+      vd = NA,
+      slope = NA,
+      time.spent = 0
+    )
+
     dat$DVnor <- dat$DV / dat$dose
     dat_fd <- dat[dat$dose_number == 1,]
     datpooled_fd <- pk.time.binning(testdat = dat_fd,
@@ -67,9 +78,8 @@ run_graphcal <- function(dat,
     )
   }
 
-  }
+  if (route=="oral"){
 
-  if (noniv_flag==1){
     graph.fd.results <- data.frame(
       ka=NA,
       cl = NA,
@@ -77,10 +87,9 @@ run_graphcal <- function(dat,
       slope = NA,
       time.spent = 0
     )
-
-    if (fdobsflag == 1) {
       start.time <- Sys.time()
       dat$DVnor <- dat$DV / dat$dose
+
       dat_fd <- dat[dat$dose_number == 1,]
       datpooled_fd <- pk.time.binning(testdat = dat_fd,
                                       nbins = nbins)
@@ -99,8 +108,6 @@ run_graphcal <- function(dat,
         slope = signif(graph.fd.output[4], 3),
         time.spent = time.spent
       )
-    }
-
   }
 
   return(graph.fd.results)
@@ -112,7 +119,7 @@ run_graphcal <- function(dat,
 #'
 #' Graphic calculation for clearance and volume of distribution from the provided data.
 #' @param dat A data frame with intravenous pharmacokinetic data, at least containing TIME and DV these two columns
-#' @param nlastpoints Number of last points to use for the linear regression of terminal slope
+#' @param nlastpoints Numeric value specifying the number of last data points used for linear regression to obtain the slope in the terminal phase. (default is \code{3} in the graphical analysis).
 #' @return A named vector containing the calculated clearance (cl), volume of distribution (vd), slope of terminal phase (slope), and concentration at the time 0 (C0).
 #' @examples
 #' dat <- data.frame(TIME = c(0.5, 1, 2, 4, 6, 8, 10), DV = c(12, 8, 5, 3, 2, 1.5, 1 ))
@@ -153,7 +160,7 @@ graphcal_iv <- function(dat,
 #' Graphical calculation of clearance and volume of distribution for oral case
 #'
 #' Graphic calculation for clearance and volume of distribution from the provided data.
-#' @param dat A data frame with intravenous pharmacokinetic data, at least containing TIME and DV these two columns
+#' @param dat A data frame with oral pharmacokinetic data, at least containing TIME and DV these two columns
 #' @param nlastpoints Number of last points to use for the linear regression of terminal slope
 #' @return A named vector containing the calculated clearance (cl), volume of distribution (vd), slope of terminal phase (slope), and concentration at the time 0 (C0).
 #' @examples
