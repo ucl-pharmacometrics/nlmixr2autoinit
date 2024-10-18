11fe977f9352872eb8c4bcbee7ad8456d362397d
ZhonghuiHuang <huangzhonghui17@163.com>
2024-10-18 17:53:00 +0100

    debug

--- R/half_life_estimated.R
+++ R/half_life_estimated.R
@@ -55,13 +55,17 @@ if (fdobsflag==1){
   dat_fd <- dat[dat$dose_number == 1, ]
   datpooled_fd <- pk.time.binning(testdat = dat_fd,
                                   nbins = nbins)
-  # half_life_estimated
-  temp1 <- tail( datpooled_fd$test.pool.normalised, n = nlastpoints)
+
+   temp1 <- tail( datpooled_fd$test.pool.normalised, n = nlastpoints)
+  if (nrow(temp1)>=nlastpoints){
   # linear regression for slope of log of DVs
   fslope <- lm(log(temp1$Conc) ~ temp1$Time)
   slope_fd <- summary(fslope)[[4]][[2]]
-  ke_fd <-  - slope_fd
-  half_life_fd <- log(2) / ke_fd # change to log(2)
+  if (slope_fd<0){
+   ke_fd <-  - slope_fd
+   half_life_fd <- log(2) / ke_fd # change to log(2)
+   }
+ }
 }
 
 if (sdflag == 0){
@@ -69,13 +73,16 @@ if (sdflag == 0){
   dat_efd <- dat[dat$dose_number != 1, ]
   datpooled_efd <- pk.time.binning(testdat = dat_efd,
                                    nbins = nbins)
-  # half_life_estimated
   temp1 <- tail( datpooled_efd$test.pool.normalised, n = nlastpoints)
+  if (nrow(temp1)>=nlastpoints){
   # linear regression for slope of log of DVs
   fslope <- lm(log(temp1$Conc) ~ temp1$Time)
   slope_efd <- summary(fslope)[[4]][[2]]
+  if (slope_efd<0){
   ke_efd <-  - slope_efd
   half_life_efd <- log(2) / ke_efd
+  }
+  }
 }
 
 # Data with samples after the first dose and repeated dose
@@ -84,17 +91,18 @@ if (fdobsflag==1 & sdflag==0){
                                    nbins = nbins)
   # half_life_estimated
   temp1 <- tail( datpooled_all$test.pool.normalised, n = nlastpoints)
+  if (nrow(temp1)>=nlastpoints){
   # linear regression for slope of log of DVs
   fslope <- lm(log(temp1$Conc) ~ temp1$Time)
   slope_all <- summary(fslope)[[4]][[2]]
+  if (slope_all<0){
   ke_all <-  - slope_all
   half_life_all <- log(2) / ke_all
+  }
+  }
 }
 
-
-
 half_life_values<-c( half_life_fd ,half_life_efd , half_life_all)
-
 # Remove negative numbers
 positive_values <-  half_life_values[ half_life_values > 0 &  !is.na( half_life_values)]
 
