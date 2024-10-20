0e2b9a9e0c8710751e7827219289cf11bdf6d112
ZhonghuiHuang <huangzhonghui17@163.com>
2024-10-20 18:01:55 +0100

    update evaluation

--- R/sim_sens_params.R
+++ R/sim_sens_params.R
@@ -15,56 +15,48 @@
 #' @export
 
 sim_sens_vmax_km <- function(dat,
-                             sim_vmax_list,
-                             sim_km_list,
+                             sim_vmax_list=NA,
+                             sim_km_list=NA,
                              estvd,
                              estcl,
                              estcmax,
-                             estka,
-                             noniv_flag) {
+                             estka=NA,
+                             noniv_flag=0) {
 
-  if (missing(estka)){
-    estka<-NA
-  }
-  if(missing(noniv_flag)){
-    noniv_flag<-0
-  }
   start.time <- Sys.time()
 
   if (missing(estvd)) {
     stop("Error: no volume of distribution for simulation was provided")
   }
 
-  if (missing(sim_vmax_list) || missing(sim_km_list)) {
-    if (missing(estcl)) {
+  if (missing(estcl)) {
       stop("Error: no estimated clearance for simulation was provided")
-    }
-    else if (missing(estcmax)) {
-      stop("Error, no estimated cmax for simuation was provided")
-    }
+  }
 
-    else{
+  if (missing(estcmax)) {
+      stop("Error, no estimated cmax for simuation was provided")
+  }
+
+  combs_df <- data.frame(vmax = sim_vmax_list, km = sim_km_list)
+
+   if(is.na(sim_vmax_list)==T || is.na(sim_km_list)==T) {
       # generate the default vman and km test list from linear kinetcs to nonlinear kinetics
       km_range <- c(4, 2, 1, 0.5, 0.25, 0.125, 0.1, 0.05) * estcmax
       # cl ~ range (0, vmax/km)
       # assume several concentration values.
       conc_range <- c(0.1, 0.25, 0.5, 0.75) * estcmax
-
       combs <- expand.grid(km_range, conc_range)
       combs_df <- data.frame(combs)
       colnames(combs_df) <- c("km", "conc")
       combs_df$vmax <- (combs_df$km + combs_df$conc) * estcl
-    }
-  }
-
-  else{
-    combs_df <- data.frame(vmax = sim_vmax_list, km = sim_km_list)
 
   }
 
-  # start simulation
+ # start simulation
   sim.vmax.km.results.all <- NULL
-  for (loopmm in 1:nrow(combs_df)) {
+
+for (loopmm in 1:nrow(combs_df)) {
+
     input.vmax <- combs_df[loopmm,]$vmax
     input.km <- combs_df[loopmm,]$km
 
@@ -91,12 +83,11 @@ sim_sens_vmax_km <- function(dat,
     )
    }
 
-    sim.lst$DV <- dat[dat$EVID == 0,]$DV
-    #  absolute percentage error  (APE)
-    sim.APE <- sum(abs(sim.lst$cp - sim.lst$DV), na.rm = T)
-    #  mean absolute percentage error  (MAPE)
-    sim.MAPE <-
-      sum(abs(sim.lst$cp - sim.lst$DV) / sim.lst$DV, na.rm = T)
+    sim.APE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[1],3)
+    sim.MAE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[2],3)
+    sim.MAPE <- signif( metrics.(pred.x = sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[3],3)
+    sim.RMSE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[4],3)
+    sim.rRMSE <- signif( metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[5],3)
 
     end.time <- Sys.time()
     time.spent <- round(difftime(end.time, start.time), 4)
@@ -106,7 +97,10 @@ sim_sens_vmax_km <- function(dat,
         vmax = input.vmax,
         km = input.km,
         sim.mm.APE = sim.APE,
+        sim.mm.MAE = sim.MAE,
         sim.mm.MAPE = sim.MAPE,
+        sim.mm.RMSE = sim.RMSE,
+        sim.mm.rRMSE = sim.rRMSE,
         time.spent = time.spent
       )
     sim.vmax.km.results.all <-
@@ -134,11 +128,11 @@ sim_sens_vmax_km <- function(dat,
 #' @export
 #'
 sim_sens_2cmpt <- function(dat,
-                           sim_vc_list,
-                           sim_vp_list,
+                           sim_vc_list=NA,
+                           sim_vp_list=NA,
                            estcl,
                            estvd,
-                           sim_q_list,
+                           sim_q_list=NA,
                            estka=NA,
                            noniv_flag=0) {
 
@@ -146,26 +140,25 @@ sim_sens_2cmpt <- function(dat,
     stop("Error: no estimated clearance for simulation was provided")
   }
 
-  if (missing(sim_vc_list) || missing(sim_vp_list)) {
-    if (missing(estvd)) {
-      stop("Error: no estimated volume of distribution for simulation was provided")
-    }
-    else{
+  if (missing(estvd)) {
+    stop("Error: no estimated volume of distribution for simulation was provided")
+  }
+
+  if (is.na(sim_vc_list)==F || is.na(sim_vp_list)==F||is.na(sim_q_list)==F) {
       # Two-compartment model
       start.time <- Sys.time()
       # from linear to nonlinear
       # 10:1, 5:1, 2:1, 1:1, 1:2, 1:5, 1:10
       vc_vp_ratio_range <- c(10, 5, 2, 1, 0.5, 0.2, 0.1)
+
       sim_vc_list <-
         signif(estvd * vc_vp_ratio_range / (vc_vp_ratio_range + 1), 3)
       sim_vp_list <-
         signif(estvd * (1 - vc_vp_ratio_range / (vc_vp_ratio_range + 1)), 3)
-    }
+
+     sim_q_list<-c(0.1,1,10,100)
   }
 
-  if (missing(sim_q_list)){
-    sim_q_list<-c(0.1,1,10,100)
-  }
 
   sim.2cmpt.results.all <- NULL
 
@@ -200,11 +193,11 @@ sim_sens_2cmpt <- function(dat,
       )
     }
 
-    sim.lst$DV <- dat[dat$EVID == 0,]$DV
-    sim.APE <- sum(abs(sim.lst$cp - sim.lst$DV), na.rm = T)
-    sim.MAPE <-
-      round(sum(abs(sim.lst$cp - sim.lst$DV) / sim.lst$DV) / nrow(sim.lst) *
-              100, 1)
+    sim.APE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[1],3)
+    sim.MAE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[2],3)
+    sim.MAPE <- signif( metrics.(pred.x = sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[3],3)
+    sim.RMSE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[4],3)
+    sim.rRMSE <- signif( metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[5],3)
 
     end.time <- Sys.time()
     time.spent <- round(difftime(end.time, start.time), 4)
@@ -223,11 +216,8 @@ sim_sens_2cmpt <- function(dat,
 
     }
   }
-
   return(sim.2cmpt.results.all)
 }
-
-
 
 #' Sensitivity analysis for a three-compartment model
 #'
@@ -260,16 +250,15 @@ sim_sens_3cmpt <- function(dat,
                            noniv_flag=0) {
 
 # default value
-if (missing(sim_q_list)){
-    sim_q_list<-c(0.1,1,10,100)
+  if (missing(estcl)) {
+    stop("Error: no estimated clearance for simulation was provided")
   }
 
-  if (missing(sim_vc_list) ||
-      missing(sim_vp_list) || missing(sim_vp2_list)) {
-    if (missing(estvd)) {
+  if (missing(estvd)) {
       stop("Error: no estimated volume of distribution for simulation was provided")
-    }
-    else{
+   }
+
+  if (is.na(sim_vc_list)==T || is.na(sim_vp_list)==T || is.na(sim_vp2_list)==T & is.na(sim_q_list)==F) {
       # generate the default parameter list
       start.time <- Sys.time()
       vc_vp_ratio_range <- c(5, 2, 1, 0.5, 0.2)
@@ -293,7 +282,9 @@ if (missing(sim_q_list)){
       sim_vp2_list <-
         signif(estvd * vp2_ratio_range / (vc_ratio_range + vp_ratio_range + vp2_ratio_range),
                3)
-    }
+
+      sim_q_list<-c(0.1,1,10,100)
+
   }
 
   sim.3cmpt.results.all <- NULL
@@ -333,11 +324,11 @@ if (missing(sim_q_list)){
     )
     }
 
-    sim.lst$DV <- dat[dat$EVID == 0,]$DV
-    sim.APE <- sum(abs(sim.lst$cp - sim.lst$DV), na.rm = T)
-    sim.MAPE <-
-      round(sum(abs(sim.lst$cp - sim.lst$DV) / sim.lst$DV) / nrow(sim.lst) *
-              100, 1)
+     sim.APE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[1],3)
+     sim.MAE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[2],3)
+     sim.MAPE <- signif( metrics.(pred.x = sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[3],3)
+     sim.RMSE <-  signif(metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[4],3)
+     sim.rRMSE <- signif( metrics.(pred.x =  sim.lst$cp ,obs.y =dat[dat$EVID == 0,]$DV )[5],3)
 
     end.time <- Sys.time()
     time.spent <- round(difftime(end.time, start.time), 4)
