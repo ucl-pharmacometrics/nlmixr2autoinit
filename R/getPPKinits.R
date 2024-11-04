e573ec86b51c33be4d14ca9494a893879d5fd1f6
ZhonghuiHuang <huangzhonghui17@163.com>
2024-11-04 03:51:29 +0000

    debug

--- R/getPPKinits.R
+++ R/getPPKinits.R
@@ -281,16 +281,14 @@ if ( is.na(simpcal.out$cl)==F & is.na(simpcal.out$vd)==F &  oral_flag ==1){
    graph_fd.RMSE <- NA
    graph_fd.rRMSE <- NA
 
-   graph.results_fd <-NA
-
-  if( fdobsflag ==1){
    graph.results_fd <- run_graphcal(
      dat = dat,
      route =   route ,
      nbins = nbins,
-     nlastpoints = nlastpoints
+     nlastpoints = nlastpoints,
+     fdobsflag=fdobsflag
    )
-  }
+
 
 ######### Predictive performance evaluation (one-compartmental parameter)#######
 # default setting
