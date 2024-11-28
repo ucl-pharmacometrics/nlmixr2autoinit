5eec1f380996a7bfb2fcc408c9967ffe0f0b8c28
ZhonghuiHuang <huangzhonghui17@163.com>
2024-11-28 19:36:00 +0000

    update default method

--- R/initsControl.R
+++ R/initsControl.R
@@ -40,7 +40,7 @@ initsControl<-function( half_life = NA,
                         trapezoidal.rule = 1,
                         nbins = 10,
                         est.method = "nls",
-                        selection.criteria="All",
+                        selection.criteria="rRMSE",
                         npdcmpt.inits.strategy= 0){
 
   return(  list(half_life =  half_life ,
