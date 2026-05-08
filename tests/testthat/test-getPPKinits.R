6724d35956ee4b5733f7f56ae403b52f275b46ae
Zhonghui Huang <94922022+ZhonghuiHuang@users.noreply.github.com>
2026-05-08 01:05:11 +0100

    Update test-getPPKinits.R
    debug

--- tests/testthat/test-getPPKinits.R
+++ tests/testthat/test-getPPKinits.R
@@ -1,15 +1,19 @@
 test_that("getPPKinits runs on example bolus data", {
   skip_on_cran()
-  
+
+  data("Bolus_1CPT", package = "nlmixr2data")
+
   out <- getPPKinits(dat = Bolus_1CPT)
-  
+
   expect_true(!is.null(out))
 })
 
 test_that("getPPKinits runs on example oral data", {
   skip_on_cran()
-  
+
+  data("Oral_1CPT", package = "nlmixr2data")
+
   out <- getPPKinits(dat = Oral_1CPT)
-  
+
   expect_true(!is.null(out))
 })
